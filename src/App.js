bd213b4a5dec357cb8544ef3268b82c169ce8d40
Petr Komlev <petr.komlev@gmail.com>
2022-09-13 10:13:11 +0300

    feat: use Message component with props in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
 
 function App() {
+  const name = 'Петр'
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Message name={name} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
